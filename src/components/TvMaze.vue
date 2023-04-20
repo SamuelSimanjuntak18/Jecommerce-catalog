2301598f3fcf47d7d93245a713fc9b3eaa306014
SamuelSimanjuntak <82867282+SamuelSimanjuntak18@users.noreply.github.com>
2023-04-20 13:24:38 +0700

    new

---
+++ src/components/TvMaze.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <div class="container">
+    <div class="row">
+      <div
+        v-for="(show, index) in shows"
+        :key="index"
+        class="col-sm mb-3"
+        style="padding-top: 10px; padding-bottom: 10px;"
+      >
+        <div class="card" style="width: 25rem; border: 1px solid gray; box-shadow: 2px 2px 4px gray;">
+          <img
+            v-if="show.image"
+            :src="show.image.medium"
+            
+            class="card-img-top"
+            :alt="`Poster for ${show.name}`"
+            style="max-width: 100%; height: auto; object-fit: cover;"
+          />
+          <div class="card-body">
+            <h5 class="card-title">{{ show.name }}</h5>
+            <button class="btn btn-primary" @click="showDetails(show)">
+              <a :href="show.url" target="_blank" class="btn btn-primary" style="text-decoration: none;">Baca Selengkapnya</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #E50914;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      shows: [],
+    };
+  },
+  mounted() {
+    fetch("http://api.tvmaze.com/search/shows?q=girls")
+      .then((response) => response.json())
+      .then((data) => {
+        this.shows = data.map((result) => ({
+          ...result.show,
+          url: result.show.officialSite || result.show.url,
+        }));
+      });
+  },
+  methods: {
+    showDetails(show) {
+      console.log(show);
+    },
+  },
+};
+
+</script>
+
+
+
+
+
+
+
+
+
